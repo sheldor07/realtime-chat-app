2c4cf4d914192e7e16138edf90f43db5594a9ed2
sheldor07 <50955708+sheldor07@users.noreply.github.com>
2023-01-23 14:23:45 +0800

    added username to msgs
    Signed-off-by: sheldor07 <50955708+sheldor07@users.noreply.github.com>

--- public/script.js
+++ public/script.js
@@ -18,10 +18,29 @@ $('#loginBox').show()
 $('#chatBox').hide()
 $('#btnStart').click(function () {
     socket.emit('login', {
-        username: $('#inpUsername').val()
+        username: $('#inpUsername').val(),
+        password:$('#inpPassword').val()
     })
+})
+
+
+socket.on('login_failed',()=>{
+    window.alert('incorrect username or password')
 })
 socket.on('logged_in', function () {
     $('#loginBox').hide()
     $('#chatBox').show()
 })
+$('#btnSend').click(function(){
+    socket.emit('msg_send', {
+                to:$('#inpToUser').val(),
+                msg: $("#inpNewMsg").val()
+            })
+            $("#inpNewMsg").val()
+})
+socket.on('msg_rcvd', (data) => {
+    console.log(data)
+        let liNewMsg = document.createElement('li')
+        liNewMsg.innerText = data.from + "->"+ data.msg
+        $('#ulMsgList').append(liNewMsg)
+    })
--- server.js
+++ server.js
@@ -7,16 +7,43 @@ const server = http.createServer(app)
 const io = socketio(server)
 
 app.use('/',express.static(__dirname+'/public'))
+let users = {yajat:'password123'}
+let socketMap = {}
 io.on('connection',(socket) =>{
     console.log("connection", socket.id)
-    socket.on('login',(data) =>{
-        socket.join(data.username)
-        socket.emit('logged_in')
-        console.log('user logged in: ',data)
-    })
+    function login(s,u){
+        s.join(u)
+        s.emit('logged_in')
+        socketMap[s.id]=u
+        console.log(socketMap)
+    }
+
     // socket.on('msg_send',(data) =>{
-    //     io.emit('msg_rcvd',data)//emit on io not socket because if socket then the data is only sent back to that socket
+    //     console.log(data)
+    //     socket.emit('msg_rcvd',data)//emit on io not socket because if socket then the data is only sent back to that socket
     // })
+    socket.on('login',(data)=>{
+        if(users[data.username]){
+            if(users[data.username]===data.password){
+                login(socket,data.username)
+            }else{
+                socket.emit('login_failed')
+            }
+        }else{
+            users[data.username] = data.password
+            login(socket,data.username)
+            
+        }
+        console.log(users)
+    })
+    socket.on('msg_send',(data) =>{
+        data.from = socketMap[socket.id]
+        if(data.to){
+            io.to(data.to).emit('msg_rcvd',data)
+        }else{
+            socket.broadcast.emit('msg_rcvd',data)
+        }
+    })
 })
 
 
