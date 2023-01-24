2eb578ae7c6cd6146eee434cddb7609517d7d7bb
sheldor07 <50955708+sheldor07@users.noreply.github.com>
2023-01-24 15:45:49 +0800

    added styling
    Signed-off-by: sheldor07 <50955708+sheldor07@users.noreply.github.com>

--- public/script.js
+++ public/script.js
@@ -14,33 +14,50 @@ let socket = io()
 //     liNewMsg.innerText = data.msg
 //     ulMsgList.appendChild(liNewMsg)
 // })
-$('#loginBox').show()
-$('#chatBox').hide()
+$('#login-container').show()
+$('#chat-container').hide()
 $('#btnStart').click(function () {
     socket.emit('login', {
         username: $('#inpUsername').val(),
-        password:$('#inpPassword').val()
+        password: $('#inpPassword').val()
     })
+    console.log("clicked")
 })
 
 
-socket.on('login_failed',()=>{
+socket.on('login_failed', () => {
     window.alert('incorrect username or password')
 })
 socket.on('logged_in', function () {
-    $('#loginBox').hide()
-    $('#chatBox').show()
+    $('.login-container').hide()
+    $('#chat-container').show()
 })
-$('#btnSend').click(function(){
+$('#btnSend').click(function () {
     socket.emit('msg_send', {
-                to:$('#inpToUser').val(),
-                msg: $("#inpNewMsg").val()
-            })
-            $("#inpNewMsg").val()
+        to: $('#inpToUser').val(),
+        msg: $("#inpNewMsg").val()
+    })
+    $("#inpNewMsg").val()
 })
 socket.on('msg_rcvd', (data) => {
     console.log(data)
-        let liNewMsg = document.createElement('li')
-        liNewMsg.innerText = data.from + "->"+ data.msg
-        $('#ulMsgList').append(liNewMsg)
-    })
+    let liNewMsg = document.createElement('li')
+    liNewMsg.innerText = data.from + "->" + data.msg
+    console.log(liNewMsg)
+    if(data.from == socket.id){
+        liNewMsg.setAttribute('sender','self')
+    }else{
+        liNewMsg.setAttribute('sender','other')
+    }
+    $('#ulMsgList').append(liNewMsg)
+})
+// $(() => {
+//     let seePassword = document.getElementById('#seePassword')
+//     seePassword.onchange = function () {
+//         if ($('#passwordCheck').val() == "on") {
+//             $("#inpPassword").prop("type", "text");
+//         } else {
+//             $("#inpPassword").prop("type", "password");
+//         }
+//     }
+// })
--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 const express = require('express')
-const http = require('http') 
+const http = require('http')
 const socketio = require('socket.io')
 
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
-app.use('/',express.static(__dirname+'/public'))
-let users = {yajat:'password123'}
+app.use('/', express.static(__dirname + '/public'))
+let users = {
+    yajat: 'password123'
+}
 let socketMap = {}
-io.on('connection',(socket) =>{
+io.on('connection', (socket) => {
     console.log("connection", socket.id)
-    function login(s,u){
+
+    function login(s, u) {
         s.join(u)
         s.emit('logged_in')
-        socketMap[s.id]=u
+        socketMap[s.id] = u
         console.log(socketMap)
     }
 
@@ -22,31 +25,32 @@ io.on('connection',(socket) =>{
     //     console.log(data)
     //     socket.emit('msg_rcvd',data)//emit on io not socket because if socket then the data is only sent back to that socket
     // })
-    socket.on('login',(data)=>{
-        if(users[data.username]){
-            if(users[data.username]===data.password){
-                login(socket,data.username)
-            }else{
+    socket.on('login', (data) => {
+        if (users[data.username]) {
+            if (users[data.username] === data.password) {
+                login(socket, data.username)
+            } else {
                 socket.emit('login_failed')
             }
-        }else{
+        } else {
             users[data.username] = data.password
-            login(socket,data.username)
-            
+            login(socket, data.username)
+
         }
         console.log(users)
     })
-    socket.on('msg_send',(data) =>{
+    socket.on('msg_send', (data) => {
         data.from = socketMap[socket.id]
-        if(data.to){
-            io.to(data.to).emit('msg_rcvd',data)
-        }else{
-            socket.broadcast.emit('msg_rcvd',data)
+        if (data.to) {
+            io.to(data.to).emit('msg_rcvd', data)
+        } else {
+            socket.broadcast.emit('msg_rcvd', data)
         }
+        socket.emit('msg_rcvd', data)
     })
 })
 
 
-server.listen(4545,() =>{
+server.listen(4545, () => {
     console.log('server started on localhost:4545')
 })
